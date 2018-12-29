eb463647cab7c4a0e93482e38ca7fe4ebc2c8b5a
jsobie03 <jonsobier@outlook.com>
2018-12-28 21:46:24 -0800

    First Project Commit

---
+++ js/planetmodal.js
@@ -0,0 +1,179 @@
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
+
+$(document).ready(function(){
+ $("#myBtn2").click(function(){
+        $("#myModal2").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn3").click(function(){
+        $("#myModal3").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn4").click(function(){
+        $("#myModal4").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn5").click(function(){
+        $("#myModal5").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn6").click(function(){
+        $("#myModal6").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn7").click(function(){
+        $("#myModal7").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn8").click(function(){
+        $("#myModal8").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn9").click(function(){
+        $("#myModal9").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn10").click(function(){
+        $("#myModal10").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn11").click(function(){
+        $("#myModal11").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn12").click(function(){
+        $("#myModal12").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn13").click(function(){
+        $("#myModal13").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn14").click(function(){
+        $("#myModal14").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn15").click(function(){
+        $("#myModal15").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn16").click(function(){
+        $("#myModal16").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn17").click(function(){
+        $("#myModal17").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn18").click(function(){
+        $("#myModal18").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn19").click(function(){
+        $("#myModal19").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn1a").click(function(){
+        $("#myModal1a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn2a").click(function(){
+        $("#myModal2a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn3a").click(function(){
+        $("#myModal3a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn4a").click(function(){
+        $("#myModal4a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn5a").click(function(){
+        $("#myModal5a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn6a").click(function(){
+        $("#myModal6a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn7a").click(function(){
+        $("#myModal7a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn8a").click(function(){
+        $("#myModal8a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn9a").click(function(){
+        $("#myModal9a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn10a").click(function(){
+        $("#myModal10a").modal();
+   });
+});
+
+$(document).ready(function(){
+ $("#myBtn11a").click(function(){
+        $("#myModal11a").modal();
+   });
+});
---
+++ js/resetbtn.js
@@ -0,0 +1,51 @@
+$("#resetButton").click(function(){
+  $("input[name='q1']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q2']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q3']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q4']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q5']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q6']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q7']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q8']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q9']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q10']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q11']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+  $("input[name='q12']:checked").prop("checked",false);
+});
+
+$("#resetButton").click(function(){
+	location.reload();
+});
